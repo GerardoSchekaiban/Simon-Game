955148456ca0a5162ab3402ecc401f4c45eb0f05
Gerardo Schekaiban <ggschekaiban@hotmail.com>
2022-03-15 12:03:48 -0600

    Add sequence function and generate game pattern

---
+++ game.js
@@ -0,0 +1,11 @@
+const gamePattern = [];
+const buttonColours = ["red", "blue", "green", "yellow"];
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    console.log(gamePattern);
+};
+
+nextSequence();
